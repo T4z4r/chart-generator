9800d0b14710d204f38ae4b38508cff935f3ddbe
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-10-24 15:07:19 +0300

    fixed landing page

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChartController;
 
+Route::get('/', [ChartController::class, 'index'])->name('home');
 Route::get('/charts', [ChartController::class, 'index'])->name('charts.index');
 Route::get('/charts/template/{type}', [ChartController::class, 'downloadTemplate'])->name('charts.template');
 Route::post('/charts/upload', [ChartController::class, 'upload'])->name('charts.upload');
 Route::get('/charts/{chart}', [ChartController::class, 'show'])->name('charts.show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
