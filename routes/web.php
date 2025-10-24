76713cf87e99e6aa75e7ea401f7bfeb96664d421
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-10-24 15:00:21 +0300

    feat: added charts routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChartController;
+
+Route::get('/charts', [ChartController::class, 'index'])->name('charts.index');
+Route::get('/charts/template/{type}', [ChartController::class, 'downloadTemplate'])->name('charts.template');
+Route::post('/charts/upload', [ChartController::class, 'upload'])->name('charts.upload');
+Route::get('/charts/{chart}', [ChartController::class, 'show'])->name('charts.show');
 
 Route::get('/', function () {
     return view('welcome');
